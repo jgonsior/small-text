74ab910c322365e54e70207e05623f58b4b1976a
Julius Gonsior <julius@gaenseri.ch>
2022-09-07 20:31:51 +0200

    raw code copied over

--- run_all.sh
+++ run_all.sh
@@ -4,7 +4,7 @@ BATCH_SIZE=25
 EXP_NAME=baseline
 RANDOM_SEED=42
 
-for UNCERTAINTY_METHOD in temp_scaling label_smoothing MonteCarlo inhibited evidential1 evidential2 bayesian ensembles trustscore model_calibration; do
+for UNCERTAINTY_METHOD in softmax temp_scaling label_smoothing MonteCarlo inhibited evidential1 evidential2 bayesian ensembles trustscore model_calibration; do
     for QUERY_STRATEGY in LC Rand Ent MM; do
         for RANDOM_SEED in 42 43 44 45 46; do
             for DATASET in trec6 ag_news subj rotten imdb; do
